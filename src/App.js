cdd2a202cb9e01b1c5d7302a560cd04dcdcdd2e4
Your Name <you@example.com>
2022-01-02 03:45:57 +0600

    error re vai

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />} />
+
             <Route path="/home" element={<Home />} />
             <Route path="/about" element={<About />} />
           </Routes>
