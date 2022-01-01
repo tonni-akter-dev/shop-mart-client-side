a8c9e95376fd2e0061cd727b5eb52e3de8ce239b
Your Name <you@example.com>
2022-01-02 01:03:16 +0600

    push

--- src/component/Shared/NavBar/NavBar.js
+++ src/component/Shared/NavBar/NavBar.js
@@ -156,6 +156,7 @@ const NavBar = () => {
                             }}
                           />
 
+
                         </span>
                       </div>
                     </div>
