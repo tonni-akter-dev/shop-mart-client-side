cf66022959383a91a37c4133b3be513908352497
Tonni Akter <tonniakterbithi@gmaile.com>
2022-01-01 17:35:15 +0600

    main

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
-        
         </Routes>
       </BrowserRouter>
     </div>
--- src/component/Home/Home/Home.js
+++ src/component/Home/Home/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import NavBar from '../../Shared/NavBar/NavBar';
 // import Banner from '../Banner/Banner';
-
 const Home = () => {
     return (
         <div>
