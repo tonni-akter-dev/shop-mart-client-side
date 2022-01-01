5b726b4529a450e93d7660b42943399051f68bd9
Tonni Akter <tonniakterbithi@gmaile.com>
2022-01-01 20:58:36 +0600

    login

--- src/component/Home/Home/Home.js
+++ src/component/Home/Home/Home.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import Footer from '../../Shared/Footer/Footer';
-import NavBar from '../../Shared/NavBar/NavBar';
-import Banner from '../Banner/Banner';
-import Gallary1 from '../Gallary1/Gallary1';
+import React from "react";
+import Footer from "../../Shared/Footer/Footer";
+import NavBar from "../../Shared/NavBar/NavBar";
+import Banner from "../Banner/Banner";
+import Gallary1 from "../Gallary1/Gallary1";
 
 const Home = () => {
-    return (
-        <div>
-            <NavBar />
-            <Banner />
-            <Gallary1/>
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <NavBar />
+      <Banner />
+      <Gallary1 />
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/component/firebase/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Spinner } from "react-bootstrap";
+import { Navigate, useLocation } from "react-router";
+import useAuth from "./../hook/useAuth";
+const PrivateRoute = ({ children, ...rest }) => {
+  let { user, isLoading } = useAuth();
+  let location = useLocation();
+
+  if (isLoading) {
+    return (
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    );
+  }
+  if (user.email) {
+    return children;
+  }
+  return <Navigate to="/login" state={{ from: location }} />;
+};
+
+export default PrivateRoute;
